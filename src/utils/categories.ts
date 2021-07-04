b93c184b9a70fd069461cd6382dac67716fea6a4
Vinicius Mazon <vnpmazon@gmail.com>
2021-07-04 20:43:20 -0300

    chore: add list of categories

---
+++ src/utils/categories.ts
@@ -0,0 +1,27 @@
+import RankedSVG from '../assets/ranked.svg';
+import DuelSVG from '../assets/duel.svg';
+import FunSVG from '../assets/fun.svg';
+import TrainingSVG from '../assets/training.svg';
+
+export const categories = [
+  {
+    id: '1',
+    title: 'Ranqueada',
+    icon: RankedSVG,
+  },
+  {
+    id: '2',
+    title: 'Duelo 1x1',
+    icon: DuelSVG,
+  },
+  {
+    id: '3',
+    title: 'Diversão',
+    icon: FunSVG,
+  },
+  {
+    id: '4',
+    title: 'Treino',
+    icon: TrainingSVG,
+  },
+]
