cbb62614f840769873e1d490e0d72d4ed1a47568
Vinicius Mazon <vnpmazon@gmail.com>
2021-07-04 12:06:21 -0300

    feat: add ButtonIcon component

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+
+import DiscordImg from '../../assets/discord.png';
+import { styles } from './styles';
+
+type Props = TouchableOpacityProps & {
+  title: string;
+}
+
+export function ButtonIcon({ title, ...rest }: Props) {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      {...rest}
+    >
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
+      <Text style={styles.title}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
