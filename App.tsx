c8a26928e9b94673e2b3661723e2b50abed35667
Vinicius Mazon <vnpmazon@gmail.com>
2021-07-04 18:46:50 -0300

    styles: add fonts and background gradient

--- App.tsx
+++ App.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
-import { SignIn } from './src/screeens/SignIn'
+import { StatusBar } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { Inter_500Medium, Inter_400Regular } from '@expo-google-fonts/inter';
+import { Rajdhani_700Bold, Rajdhani_500Medium } from '@expo-google-fonts/rajdhani';
+
+import { SignIn } from './src/screens/SignIn';
+import { Background } from './src/components/Background';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    < SignIn />
+    <Background>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      < SignIn />
+    </Background>
   );
 }
