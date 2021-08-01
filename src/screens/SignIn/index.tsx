a35cf950b26e2efe1a76f3d5fe0d0426ba92c5b6
Vinicius Mazon <vnpmazon@gmail.com>
2021-08-01 10:52:12 -0300

    fix: fix background

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -5,6 +5,8 @@ import illustrationImg from '../../assets/illustration.png';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { useNavigation } from '@react-navigation/native';
 
+import { Background } from '../../components/Background';
+
 export function SignIn() {
   const navigation = useNavigation();
 
@@ -13,27 +15,29 @@ export function SignIn() {
   }
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={illustrationImg}
-        style={styles.image}
-        resizeMode="stretch"
-      />
-
-      <View style={styles.content}>
-        <Text style={styles.title}>
-          Conecte-se {'\n'} e organize suas jogatinas
-        </Text>
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
-          favoritos com seus amigos
-        </Text>
-
-        <ButtonIcon
-          title="Entre com Discord"
-          onPress={handleSignIn}
+    <Background>
+      <View style={styles.container}>
+        <Image
+          source={illustrationImg}
+          style={styles.image}
+          resizeMode="stretch"
         />
+
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Conecte-se {'\n'} e organize suas jogatinas
+          </Text>
+          <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games {`\n`}
+            favoritos com seus amigos
+          </Text>
+
+          <ButtonIcon
+            title="Entre com Discord"
+            onPress={handleSignIn}
+          />
+        </View>
       </View>
-    </View>
+    </Background>
   );
 }
