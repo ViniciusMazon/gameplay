6815b841a4797e7ddc23a755429284c887724876
Vinicius Mazon <vnpmazon@gmail.com>
2021-08-01 19:31:34 -0300

    fix: change style

--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -4,7 +4,7 @@ import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
   container: {
-    width: '78%',
+    width: '75%',
     height: 1,
     backgroundColor: theme.colors.secondary40,
     marginTop: 2,
