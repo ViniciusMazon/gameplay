ef40d70cc0edc5aa0e0760ad455dee283dbe1914
Vinicius Mazon <vnpmazon@gmail.com>
2021-08-01 12:21:08 -0300

    feat: add appointment create route

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -5,6 +5,7 @@ import { theme } from '../global/styles/theme';
 import { SignIn } from '../screens/SignIn';
 import { Home } from '../screens/Home';
 import { AppointmentDetails } from '../screens/AppointmentDetails';
+import { AppointmentCreate } from '../screens/AppointmentCreate';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -21,6 +22,7 @@ export function AuthRoutes() {
       <Screen name='SignIn' component={SignIn} />
       <Screen name='Home' component={Home} />
       <Screen name='AppointmentDetails' component={AppointmentDetails} />
+      <Screen name='AppointmentCreate' component={AppointmentCreate} />
     </Navigator>
   );
 }
