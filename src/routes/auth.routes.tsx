96b722e92c908829d56d2ea4daabb9bfcf7cf48f
Vinicius Mazon <vnpmazon@gmail.com>
2021-08-01 10:53:26 -0300

    feat: add appointment details route

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { theme } from '../global/styles/theme';
 
 import { SignIn } from '../screens/SignIn';
 import { Home } from '../screens/Home';
+import { AppointmentDetails } from '../screens/AppointmentDetails';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -12,12 +14,13 @@ export function AuthRoutes() {
       headerMode='none'
       screenOptions={{
         cardStyle: {
-          backgroundColor: 'transparent',
+          backgroundColor: theme.colors.secondary100,
         }
       }}
     >
       <Screen name='SignIn' component={SignIn} />
       <Screen name='Home' component={Home} />
+      <Screen name='AppointmentDetails' component={AppointmentDetails} />
     </Navigator>
   );
 }
