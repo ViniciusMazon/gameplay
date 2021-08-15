455273317f726324442a38a859151bcc16b4d9d5
Vinicius Mazon <vnpmazon@gmail.com>
2021-08-15 20:34:03 -0300

    fix: cancel sign in error

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -30,7 +30,8 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
   params: {
-    access_token: string;
+    access_token?: string;
+    error?: string;
   }
 }
 
@@ -48,7 +49,7 @@ function AuthProvider({ children }: AuthProviderProps) {
       const { type, params } = await AuthSession
         .startAsync({ authUrl }) as AuthorizationResponse;
 
-      if (type === 'success') {
+      if (type === 'success' && !params.error) {
         api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
         const userInfo = await api.get('/users/@me');
--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -64,7 +64,7 @@ export function AppointmentDetails() {
         renderItem={({ item }) => (
           <Member data={item} />
         )}
-        ItemSeparatorComponent={() => <ListDivider />}
+        ItemSeparatorComponent={() => <ListDivider isCentered />}
         style={styles.members}
       />
 
