f490be960239ad9786d300c965015106ab99e649
Vinicius Mazon <vnpmazon@gmail.com>
2021-08-21 08:40:48 -0300

    feat: add new appointment

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -2,25 +2,35 @@ import React, { useState } from 'react';
 import { View, Text, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import uuid from 'react-native-uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
 import { CategorySelect } from '../../components/CategorySelect';
 import { GuildIcon } from '../../components/GuildIcon';
 import { Button } from '../../components/Button';
-
-import { styles } from './styles';
-import { theme } from '../../global/styles/theme';
 import { SmallInput } from '../../components/SmallInput';
 import { TextArea } from '../../components/TextArea';
 import { Guilds } from '../Guilds';
 import { ModalView } from '../../components/ModalView';
 import { GuildProps } from '../../components/Guild';
+import { COLLECTION_APPOINTMENTS } from '../../configs/database';
+
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
+import { useNavigation } from '@react-navigation/core';
 
 export function AppointmentCreate() {
+  const navigation = useNavigation();
   const [category, setCategory] = useState('');
   const [toggleGuildModal, setToggleGuildModal] = useState(false);
   const [selectedGuild, setSelectedGuild] = useState<GuildProps>({} as GuildProps);
+  const [day, setDay] = useState('');
+  const [month, setMonth] = useState('');
+  const [minute, setMinute] = useState('');
+  const [hour, setHour] = useState('');
+  const [description, setDescription] = useState('');
 
   function handleToggleGuildModal() {
     setToggleGuildModal(!toggleGuildModal);
@@ -33,6 +43,32 @@ export function AppointmentCreate() {
 
   function handleSelectCategory(categoryId: string) {
     setCategory(categoryId);
+  }
+
+  async function handleSave() {
+    if (
+      !selectedGuild || !category || !month || !day || !hour || !minute || !description
+    ) {
+      return
+    }
+
+    const newAppointment = {
+      id: uuid.v4(),
+      guild: selectedGuild,
+      category,
+      date: `${day}/${month} às ${hour}:${minute}h`,
+      description,
+    };
+
+    const storage = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS)
+    const appointments = storage ? JSON.parse(storage) : [];
+
+    await AsyncStorage.setItem(COLLECTION_APPOINTMENTS, JSON.stringify([
+      ...appointments,
+      newAppointment
+    ]));
+
+    navigation.navigate('Home');
   }
 
   return (
@@ -82,18 +118,18 @@ export function AppointmentCreate() {
               <View>
                 <Text style={[styles.label, { marginBottom: 12 }]}>Dia e mês</Text>
                 <View style={styles.column}>
-                  <SmallInput maxLength={2} />
+                  <SmallInput maxLength={2} onChangeText={setDay} />
                   <Text style={styles.divider}>/</Text>
-                  <SmallInput maxLength={2} />
+                  <SmallInput maxLength={2} onChangeText={setMonth} />
                 </View>
               </View>
 
               <View>
                 <Text style={[styles.label, { marginBottom: 12 }]}>Hora e minuto</Text>
                 <View style={styles.column}>
-                  <SmallInput maxLength={2} />
+                  <SmallInput maxLength={2} onChangeText={setHour} />
                   <Text style={styles.divider}>:</Text>
-                  <SmallInput maxLength={2} />
+                  <SmallInput maxLength={2} onChangeText={setMinute} />
                 </View>
               </View>
             </View>
@@ -108,10 +144,11 @@ export function AppointmentCreate() {
               maxLength={100}
               numberOfLines={5}
               autoCorrect={false}
+              onChangeText={setDescription}
             />
 
             <View style={styles.footer}>
-              <Button title='Agendar' />
+              <Button title='Agendar' onPress={handleSave} />
             </View>
           </View>
         </ScrollView>
